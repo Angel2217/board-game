b1579f804c850018f505f0e01a2f075be61137db
Angel2217 <53708383+Angel2217@users.noreply.github.com>
2019-09-05 10:06:24 +0200

    Update 05/09

--- js.js
+++ js.js
@@ -9,7 +9,6 @@ class App {
 }
 
 
-
 class Square {
   constructor(id) {
     this.id = id;
@@ -30,22 +29,33 @@ class Square {
   }
 
   get cash() {
-    let elem = $('#' + this.id);
-    return $(elem).hasClass('cash');
+    let amount = 0;
+    let cashDiv = $('#'+ this.id + '.cash');
+    if (cashDiv) {
+        amount = Number( $(cashDiv).text() );  
+    }
+    return amount;
   }
 
+    
+  set cash(amount) {
+    let elem = $('#' + this.id);
+    $(elem).append('<div class="cash">' + amount + '</div>');
+  }
 
-  set cash(bool) {
+  get player() {
+    let elem = $('#' + this.id);
+    return elem.player
+  }
+
+  set player(bool) {
     let elem = $('#' + this.id);
     if (bool) {
-      $(elem).addClass('cash');
-    } else {
-      $(elem).removeClass('cash');
+      $(elem).append('<div class="player"> </div>');
     }
   }
 
 } // end of Square
-
 
 
 
@@ -55,6 +65,7 @@ class Board {
     this.table = this.createTable();
     this.block = this.blockRandomSquare();
     this.cash = this.addCash();
+    this.player = this.placePlayer();
   }
 
 
@@ -89,20 +100,59 @@ class Board {
   }
 
   addCash() {
-    var cashCount = 0;
-    while (cashCount < 5) {
+    var cash = [20, 30, 40, 50, 60];
+    while (cash.length > 0) {
       let r = Math.floor(Math.random() * this.size);
       let c = Math.floor(Math.random() * this.size);
       let tdId = `sq_${r}_${c}`;
       let square = new Square(tdId);
       if (!square.cash && !square.blocked) {
-        square.cash = true;
-        cashCount++
+        let amount = cash.pop();
+        square.cash = amount;
       }
     }
   }
 
+  placePlayer() {
+    let r = Math.floor(Math.random() * this.size);
+    let c = Math.floor(Math.random() * this.size);
+    let tdId = `sq_${r}_${c}`;
+    let square = new Square(tdId);
+    let player = new Player();
+    if (!square.cash && !square.blocked) {
+      square.player = true;
+      player.name = 'Angelica';
+      player.money = 0;
+    }
+  }
+
+
 } // end of Board 
 
+
+
+class Player {
+  constructor(name) {
+    this.name = name;
+  }
+
+  get name() {
+    return this.name
+  }
+
+  set name(name) {
+    $(".player").append('<div class="name">' + name + '</div>');
+  }
+
+
+  get money() {
+    return this.money
+  }
+
+  set money(money) {
+    $(".player").append('<div class="money">' + money + '</div>');
+  }
+
+} // end of Player
 
 let myApp = new App(5);
